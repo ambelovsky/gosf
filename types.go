72add02ea8a6d72a3c2635669b6e5b8f2be6f2c6
Aaron Belovsky <ambelovsky@gmail.com>
2019-04-25 22:28:01 -0500

    Refactoring
    Refactored standard Message
    Added connect/disconnect options for aspects

--- types.go
+++ types.go
@@ -4,15 +4,11 @@ import io "github.com/graarh/golang-socketio"
 
 // Message - Standard message type for Socket communications
 type Message struct {
-	Status string `json:"status"`
+	ID      int  `json:"id,omitempty"`
+	Success bool `json:"success"`
 
-	ID int `json:"id,omitempty"`
-
-	Key     string `json:"key,omitempty"`
-	Version string `json:"version,omitempty"`
-
-	Message string `json:"message,omitempty"`
-	Data    string `json:"data,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 // Session is a data store for a single client connection
@@ -24,6 +20,10 @@ type Session struct {
 type Plugin interface {
 	Activate(app *map[string]interface{})
 	Deactivate(app *map[string]interface{})
+
+	Connect(app *map[string]interface{})
+	Disconnect(app *map[string]interface{})
+
 	PreReceive(clientMessage *Message)
 	PostReceive(clientMessage *Message)
 	PreRespond(clientMessage *Message, serverMessage *Message)
