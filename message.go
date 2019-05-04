f8f9f4e61975272a832a4e2405743cf44df51d8e
Aaron Belovsky <ambelovsky@gmail.com>
2019-05-04 12:08:29 -0500

    Strict interface enforcement

--- message.go
+++ message.go
@@ -1,11 +1,5 @@
 package gosf
 
-import (
-	"reflect"
-	"strings"
-	"unicode"
-)
-
 // Message - Standard message type for Socket communications
 type Message struct {
 	ID int `json:"id,omitempty"`
@@ -13,8 +7,8 @@ type Message struct {
 	Success bool   `json:"success"`
 	Text    string `json:"text,omitempty"`
 
-	Meta map[string]interface{} `json:"meta,omitempty"`
-	Body map[string]interface{} `json:"body,omitempty"`
+	Meta interface{} `json:"meta,omitempty"`
+	Body interface{} `json:"body,omitempty"`
 }
 
 // WithoutMeta removes meta information before returning a copy of the message
@@ -37,44 +31,7 @@ func NewSuccessMessage(text string, body interface{}) *Message {
 	if text != "" {
 		m.Text = text
 	}
-	m.Body = convertToJSONMap(body)
+	m.Body = body
 
 	return m
-}
-
-func convertToJSONMap(data interface{}) map[string]interface{} {
-	jsonMap := make(map[string]interface{})
-
-	arg := reflect.ValueOf(data)
-	for j := 0; j < arg.NumField(); j++ {
-		value := arg.Field(j).Interface()
-		valueType := arg.Field(j).Kind()
-
-		if value == nil {
-			continue
-		}
-
-		name := arg.Type().Field(j).Name
-		nameBytes := []byte(name)
-		nameBytes[0] = byte(unicode.ToLower(rune(nameBytes[0])))
-		name = string(nameBytes)
-
-		tag := arg.Type().Field(j).Tag.Get("json")
-		if tag != "" {
-			tagParts := strings.Split(tag, ",")
-			if len(tagParts) > 0 {
-				name = tagParts[0]
-			}
-
-			// ignore requests for omitempty, this function omits empty by default
-			// no check for other tag parts
-		}
-
-		if valueType == reflect.Struct {
-			jsonMap[name] = convertToJSONMap(&value)
-		} else {
-			jsonMap[name] = value
-		}
-	}
-	return jsonMap
 }
