3d655c8d980683a952d5c40cf4a15c1c2e821a76
Aaron Belovsky <ambelovsky@gmail.com>
2019-05-04 12:48:23 -0500

    Added convenience functions for converting structs to and from maps

---
+++ encoder.go
@@ -0,0 +1,16 @@
+package gosf
+
+import (
+	"github.com/fatih/structs"
+	"github.com/mitchellh/mapstructure"
+)
+
+// StructToMap converts the given structure into a map[string]interface{}
+func StructToMap(input interface{}) map[string]interface{} {
+	return structs.Map(input)
+}
+
+// MapToStruct converts the given map[string]interface{} into a struct
+func MapToStruct(input map[string]interface{}, output interface{}) error {
+	return mapstructure.Decode(input, output)
+}
--- getdeps.sh
+++ getdeps.sh
@@ -1,1 +1,3 @@
 go get -u "github.com/ambelovsky/gosf-socketio"
+go get -u "github.com/mitchellh/mapstructure"
+go get -u "github.com/fatih/structs"
